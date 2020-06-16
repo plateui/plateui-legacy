a3e642ee78ebcca18a74adec24f4afc392bea9b8
Hsiaoming Yang <me@lepture.com>
2020-06-16 21:38:03 +0900

    Move mock data into demo folder

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import plate from './index'
 
 window.plate = plate
 
-const endpoint = '/plate.json'
+const endpoint = '/demo/plate.json'
 // const endpoint = '/api/_plate/_/main'
 
 plate.init(endpoint).then(app => {
